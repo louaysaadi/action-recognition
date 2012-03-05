952b6e5a1a1be209d55afe0cce410de67f26661d
clblacksmith@163.com <clblacksmith@163.com@babf760f-17fb-7e02-aba6-2736b1e40886>
2012-03-05 14:41:53 +0000

    /home/hcl/ar.log

--- src/CSimleMethod.cpp
+++ src/CSimleMethod.cpp
@@ -12,24 +12,33 @@ CSimleMethod::~CSimleMethod()
 
  IplImage *CSimleMethod::extractForeground( IplImage * img, const IplImage * grd )
  {
-     //cvAbsDiff( img->imageData, grd->imageData, img->imageData );
+     //IplImage * nimg = cvCloneImage( img );
+     //cvAbsDiff( img->imageData, grd->imageData, nimg->imageData );
+
      int i,j;
+     long total = 0;
      char pv;
+     static int val = 0;
      for( i = 0; i < img->height ; i ++ )
      {
          for( j = 0; j < img->widthStep; j++ )
          {
-             pv = ( img->imageData[ i * img->widthStep + j ] - grd->imageData[ i * img->widthStep + j ] );
-             if ( pv < 10 )
+             pv = (  img->imageData[ i * img->widthStep + j ] - grd->imageData[ i * grd->widthStep + j ] );
+
+             if ( pv < val * 5 )
              {
-                  //img->imageData[ i * img->widthStep + j ] = img->imageData[ i * img->widthStep + j ];
+                  img->imageData[ i * img->widthStep + j ] = '\0';
              }
              else
              {
-                 img->imageData[ i * img->widthStep + j ] = '\0';
+                 //img->imageData[ i * img->widthStep + j ]
              }
 
+             //img->imageData[ i * img->widthStep +  j ] = abs(pv);
+             total += abs( pv );
          }
      }
+     val = total / img->height / img->widthStep;
+
      return img;
  }
