0cf77359be223a5405b338bfe30b56e7951f9088
clblacksmith@163.com <clblacksmith@163.com@babf760f-17fb-7e02-aba6-2736b1e40886>
2012-03-08 03:48:37 +0000

    null

--- trunk/src/CSimleMethod.cpp
+++ trunk/src/CSimleMethod.cpp
@@ -21,24 +21,32 @@ CSimleMethod::~CSimleMethod()
      static int val = 0;
      for( i = 0; i < img->height ; i ++ )
      {
-         for( j = 0; j < img->widthStep; j++ )
+         for( j = 0; j < img->widthStep; j += 3 )
          {
              pv = (  img->imageData[ i * img->widthStep + j ] - grd->imageData[ i * grd->widthStep + j ] );
+             pv += (  img->imageData[ i * img->widthStep + j + 1 ] - grd->imageData[ i * grd->widthStep + j + 1 ] );
+             pv += (  img->imageData[ i * img->widthStep + j + 2 ] - grd->imageData[ i * grd->widthStep + j + 2 ] );
 
-             if ( pv < val * 5 )
+             if ( pv < val  )
              {
-                  img->imageData[ i * img->widthStep + j ] = '\0';
+
+                  img->imageData[ i * img->widthStep + j ] = 0;
+                  img->imageData[ i * img->widthStep + j + 1 ] = 0;
+                  img->imageData[ i * img->widthStep + j + 2 ] = 0;
              }
              else
              {
                  //img->imageData[ i * img->widthStep + j ]
+                 img->imageData[ i * img->widthStep + j ] = 255;
+                 img->imageData[ i * img->widthStep + j + 1 ] = 255;
+                 img->imageData[ i * img->widthStep + j + 2 ] = 255;
              }
 
              //img->imageData[ i * img->widthStep +  j ] = abs(pv);
              total += abs( pv );
          }
      }
-     val = total / img->height / img->widthStep;
+     val = total / img->height / img->width;
 
      return img;
  }
